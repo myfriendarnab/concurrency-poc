cd5e1c87670c7be3d5f6eabd9e0cc84b086e6489
Arnab Bhattacharya <Arnab.Bhattacharya@maersk.com>
2021-08-27 11:17:10 +0530

    code changes

--- DataFlowProcessing/Program.cs
+++ DataFlowProcessing/Program.cs
@@ -13,6 +13,14 @@ namespace DataFlowProcessing
         {
             Console.WriteLine("Hello World!");
 
+            var prg = new Program();
+            await prg.TransFormActionFirst();
+
+            Console.ReadLine();
+        }
+
+        private async Task TransFormActionFirst()
+        {
             var block1 = new TransformBlock<int, string>(i =>
                 {
                     //api call
@@ -30,38 +38,33 @@ namespace DataFlowProcessing
                 Console.WriteLine(s + "***");
             });
 
-            var block3 = new BufferBlock<string>(new ExecutionDataflowBlockOptions { BoundedCapacity = 5 });
-            block1.LinkTo(
-                block3
-                , new DataflowLinkOptions{PropagateCompletion = true}
-                , s => true
-                );
-            block1.LinkTo(DataflowBlock.NullTarget<string>());
+            // var block3 = new BufferBlock<string>(new ExecutionDataflowBlockOptions { BoundedCapacity = 5 });
+            // block1.LinkTo(
+            //     block3
+            //     , new DataflowLinkOptions{PropagateCompletion = true}
+            //     , s => true
+            //     );
+            //block1.LinkTo(DataflowBlock.NullTarget<string>());
 
             try
             {
                 using (block1.LinkTo(block2))
                 {
-                    Enumerable.Range(1, 10).ToList().ForEach(i =>
-                    {
-                        block1.SendAsync(i);
-                    });
+                    Enumerable.Range(1, 10).ToList().ForEach(i => { block1.SendAsync(i); });
 
                     block1.Complete();
                     await block2.Completion;
                 }
-                block3.Complete();
-                block3.TryReceiveAll(out IList<string> items);
+                // block3.Complete();
+                // block3.TryReceiveAll(out IList<string> items);
 
-                Console.WriteLine(items.Count);
+                // Console.WriteLine(items.Count);
             }
             catch (AggregateException aex)
             {
                 foreach (var innerExceptions in aex.Flatten().InnerExceptions)
                     Console.WriteLine(innerExceptions.Message);
             }
-
-            Console.ReadLine();
         }
     }
 }
