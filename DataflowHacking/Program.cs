ff8781394075e167b04102f74e78c0346cb5c6e9
James Murphy <james.murphy@maersk.com>
2021-08-27 09:25:13 +0100

    Multi stage pipeline that calls out in parallel
    - There is a concurrent call out (Task.Delay) to the external service
    - There is a single post-processor that handles the update
    
    The update can be blocking (set NOTIFY_EVERY to 5 to see effect)

--- DataflowHacking/Program.cs
+++ DataflowHacking/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading;
@@ -13,49 +13,69 @@ namespace DataflowHacking
         static async Task Main(string[] args)
         {
             // This isn't "batch" size, but its close enough
-            const int BATCH_SIZE = 5;
+            const int BATCH_SIZE = 15;
+            const int NOTIFY_EVERY = BATCH_SIZE; // Because of the timings, you can get the notify blocking if this is too small relative to batch size
 
-            var collect = new ConcurrentBag<string>();
             var processedCount = 0;
 
             Console.WriteLine("Dataflow hacking");
 
-            var fred = Enumerable.Range(1, 23);
+            var fred = Enumerable.Range(1, 123);
 
-            //            var transformer = new TransformBlock<int, string>(i => i.ToString());
-            var action = new ActionBlock<int>(async i =>
-                {
-                    var delay = (6 - (i % 5)) * 250;
-                    Console.WriteLine($">> Processing {i} with delay {delay}");
+            var transformer = new TransformBlock<int, string>(async i =>
+            {
+                var delay = (6 - (i % 5)) * 250;
+                Console.WriteLine($">> Processing {i} with delay {delay}");
 
-                    await Task.Delay(delay);
-                    collect.Add($"Item {i}");
-                    Console.WriteLine($"<< Processed {i}");
-                    var total = Interlocked.Increment(ref processedCount);
-                    if (total % 5 == 0)
-                    {
-                        Console.WriteLine($"=== Updating total: {total}");
-                        await Task.Delay(333);  // Simulate writing to database and sending notification
-                        Console.WriteLine($"=== Total updated: {total}");
-                    }
-                },
-                new ExecutionDataflowBlockOptions() {  MaxDegreeOfParallelism = BATCH_SIZE }
+                await Task.Delay(delay);
+
+                // Result should come from the called task
+                var result = $"Item {i}";
+
+                Console.WriteLine($"<< Processed {i}");
+
+                return result;
+            },
+                new ExecutionDataflowBlockOptions() { MaxDegreeOfParallelism = BATCH_SIZE }
             );
 
+            var notifyTransformer = new TransformBlock<string, string>(async s =>
+            {
+                Console.WriteLine("Notify");
+                var total = Interlocked.Increment(ref processedCount);
+
+                if (total % NOTIFY_EVERY == 0)
+                {
+                    Console.WriteLine($"=== Updating total: {total}");
+                    await Task.Delay(333);  // Simulate writing to database and sending notification
+                    Console.WriteLine($"=== Total updated: {total}");
+                }
+
+                return s;
+            }
+            );
+
+            transformer.LinkTo(notifyTransformer, new DataflowLinkOptions() { PropagateCompletion = true });
             var sw = Stopwatch.StartNew();
 
-            foreach(var i in fred)
+            foreach (var i in fred)
             {
-                await action.SendAsync(i);
+                await transformer.SendAsync(i);
             }
 
             Console.WriteLine("*** Complete");
-            action.Complete();
+            transformer.Complete();
+
+            var itemList = new List<string>();
+            while (await notifyTransformer.OutputAvailableAsync())
+            {
+                itemList.Add(await notifyTransformer.ReceiveAsync());
+            }
 
             Console.WriteLine("*** Await Completion");
-            await action.Completion;
+            await notifyTransformer.Completion;
 
-            var itemList = collect.ToList();
+            // var itemList = collect.ToList();
 
             sw.Stop();
 
